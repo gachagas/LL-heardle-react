827157b4b754db423cf3450a3a54955e003bb63e
gachagas <gerlito.chagas@gmail.com>
2022-09-16 00:06:29 +0800

    add noto serif display font

---
+++ pages/SVGs/index.tsx
@@ -0,0 +1,13 @@
+import about from "./about.svg";
+import heart from "./heart.svg";
+import question from "./question.svg";
+import stats from "./stats.svg";
+
+const icons = {
+  about: about,
+  heart: heart,
+  question: question,
+  stats: stats,
+};
+
+export default icons;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      serif: ["Noto Serif Display, ui-serif, Georgia"],
+    },
     extend: {},
   },
   plugins: [],
