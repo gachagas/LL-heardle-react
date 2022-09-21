950f1203c2aa2000ee85da5a75ecbb2e08e83f9b
gachagas <gerlito.chagas@gmail.com>
2022-09-21 22:19:58 +0800

    migrate to CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,99 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+
+import Answerbox from "./components/Answerbox";
+import iconSVG from "./components/SVGs/index";
+
 import "./App.css";
 
+const answers = [
+  { key: 0, song: "SKIPPED" },
+  { key: 1, song: "Minami Kotori - Spicaterrible / スピカテリブル" },
+  { key: 2, song: "SKIPPED" },
+  { key: 3, song: "SKIPPED" },
+  { key: 4, song: "" },
+  { key: 5, song: "" },
+];
+
 const App: React.FunctionComponent = () => {
+  // eslint-disable-next-line
+  const [tryNumber, setTryNum] = useState(5);
+
+  const answerList = answers.map((ans) => (
+    <Answerbox
+      id={ans.key}
+      key={ans.key}
+      song={ans.song}
+      tryNumber={tryNumber}
+    />
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hey! <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <head>LL Heardle!</head>
+      <main style={{ height: "100vh" }} className="bg-custom-bg flex flex-col">
+        <div className="border-b custom-border-color-selected">
+          <div className="max-w-3xl mx-auto">
+            <header className="flex justify-evenly text-custom-fgcolor p-3 items-center ">
+              <div className="flex-1 ">
+                <div className="flex flex1 items-end justify-start shrink">
+                  <div className="p-2 ">{iconSVG.about}</div>
+                  <div className="p-2">{iconSVG.heart}</div>
+                </div>
+              </div>
+              <div className="flex-1 ">
+                <h1 className="font-serif text-3xl font-bold text-center flex-grow">
+                  LoveLive! Heardle
+                </h1>
+              </div>
+              <div className="flex flex-1 justify-end">
+                <div className="p-2">{iconSVG.stats}</div>
+                <div className="p-2">{iconSVG.question}</div>
+              </div>
+            </header>
+          </div>
+        </div>
+        <div className="p-3 flex flex-col max-w-screen-sm flex-grow mx-auto w-full">
+          {answerList}
+        </div>
+        <div>Turn up the volume container</div>
+        <div className="justify-end">Progress bar Container</div>
+        <div>Play button Container</div>
+        <div className="m-auto max-w-screen-sm w-full ">
+          <div className="mx-3 mb-3">
+            <form>
+              <div className="flex w-full border custom-border-color-unselected">
+                {/* {iconSVG.magnifying} */}
+                <input
+                  className="w-full border-none bg-transparent py-3 pr-3 pl-9 "
+                  type="text"
+                  name="name"
+                  placeholder="Know it? Search for the artist/title"
+                />
+                <div className="relative right-3 top-4">
+                  {/* {iconSVG.magnifying} */}
+                </div>
+              </div>
+              <div className="flex justify-between pt-3">
+                <button
+                  type="button"
+                  value="Submit"
+                  className="flex-none px-2 py-2 tracking-widest font-bold text-sm skip-bg"
+                >
+                  SKIP (+4s)
+                </button>
+
+                <button
+                  type="button"
+                  value="Submit"
+                  className=" flex-none px-2 py-2 tracking-widest font-bold text-sm submit-bg"
+                >
+                  SUBMIT
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
